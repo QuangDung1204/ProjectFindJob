7f614f9d43dfa044544dc1ff09a4544ceb8933c4
QuangDung1204 <quangdung12042004@gmail.com>
2025-04-09 16:29:52 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()
+    react(),
   ],
   css: {
-    postcss: './postcss.config.cjs', // Đảm bảo đường dẫn đúng
+    postcss: './postcss.config.cjs',
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 });
