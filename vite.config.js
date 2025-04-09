c064f6363fed0eb86cdbf91e2c17b76ad3354817
QuangDung1204 <quangdung12042004@gmail.com>
2025-04-09 16:36:08 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -13,9 +13,14 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8080', 0
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '/api/v1/auth/register': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/v1\/auth\/register/, '/v1/auth/register'),
       },
     },
   },
